1f51f1b121f711f0b82a10091ddc9b8622329ff5
Reo Chen <crlwxy@gmail.com>
2016-12-02 15:18:41 +0800

    reochen

---
+++ files/news/data.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	"use strict";
+	var newsPageNum=1;
+	var news=$(".news");	$("#selectedPage").css("top","580px");
+	for(var i=0;i<news.length;i++){
+		if(i!==newsPageNum-1){
+			$(news[i]).hide();
+		}
+	}
+
+});
