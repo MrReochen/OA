996ab2ce1dc667d7c8c18efc3e381419bcf906a7
mmmmayi <657818050@qq.com>
2016-12-02 17:34:14 +0800

    mmmmayi

---
+++ files/contract3/con-add.js
@@ -0,0 +1,17 @@
+/* Sass Document */
+$(document).ready(function(){
+	"use strict";
+	$("#pic14").hide();
+	$("#pic12").click(function(){
+		window.location.href="applyforholiday.html";
+	});
+	$("#pic13").click(function(){
+		window.location.href="applyforreimbursement.html";
+	});
+	$("#pic9").click(function(){
+		$("#pic14").show();
+	});
+	$("#pic14").click(function(){
+		$("#pic14").hide();
+	});
+});
