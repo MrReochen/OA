ac24a353d0f56c75da05bcbfde0cad12a183d4a3
mmmmayi <657818050@qq.com>
2016-12-05 15:11:31 +0800

    mmmmayi

--- files/applyforholiday/add-holiday.js
+++ files/applyforholiday/add-holiday.js
@@ -1,9 +1,8 @@
 /* Sass Document */
 $(document).ready(function(){
 	"use strict";
-	$("#pic14").hide();
 	$("#pic12").click(function(){
-		window.location.href="applyforholiday.html";
+		window.location.href="addfile.html";
 	});
 	$("#pic3").click(function(){
 		window.location.href="home.html";
@@ -12,12 +11,7 @@ $(document).ready(function(){
 		window.location.href="home.html";
 	});
 	$("#pic13").click(function(){
-		window.location.href="applyforreimbursement.html";
-	});
-	$("#pic9").click(function(){
-		$("#pic14").show();
-	});
-	$("#pic14").click(function(){
-		$("#pic14").hide();
+		window.location.href="contract3.html";
 	});
 });
+
