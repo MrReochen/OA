ba81c2349d1714cc51879d6b11e18510b91b35b6
mmmmayi <657818050@qq.com>
2016-12-05 17:15:26 +0800

    mmmmayi

---
+++ files/addfile/addfile.js
@@ -0,0 +1,27 @@
+/* Sass Document */
+$(document).ready(function(){
+	"use strict";
+	$("#pic15").hide();
+	$("#pic12").click(function(){
+		window.location.href="applyforreimbursement.html";
+	});
+	$("#pic3").click(function(){
+		window.location.href="home.html";
+	});
+	$("#pic4").click(function(){
+		window.location.href="home.html";
+	});
+	$("#pic13").click(function(){
+		window.location.href="applyforholiday.html";
+	});
+	$("#pic9").click(function(){
+		$("#pic15").show();
+	});
+	$("#pic15").click(function(){
+		$("#pic15").hide();
+	});
+	$("#pic10").click(function(){
+		$("#pic15").show();
+	});
+});
+
