ed3e8d834ac9b2b63b477fb58c2fb16589b7fc8b
Reo Chen <crlwxy@gmail.com>
2016-12-05 17:02:28 +0800

    reochen

--- files/calendar/data.js
+++ files/calendar/data.js
@@ -1,1 +1,9 @@
 // JavaScript Document
+$(document).ready(function(){
+	"use strict";
+	$("#selectedPage").css("top","772px");
+	var mySwiper=new Swiper('.swiper-container',{
+		loop:true
+	});
+	
+});
--- files/news/data.js
+++ files/news/data.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
 	"use strict";
+	$("#selectedPage").css("top","580px");
 	var mySwiper = new Swiper('.swiper-container',{
 		pagination : '.swiper-pagination',
 		//pagination : '#swiper-pagination1',
 		paginationClickable :true,
-		loop:true,
+		loop:true
 	});
 	$("#nextPage").click(function(){
 		mySwiper.slideNext();
