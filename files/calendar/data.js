19c1e19eba680153d6b2ff1033731f654e11ddba
Reo Chen <crlwxy@gmail.com>
2016-12-06 10:44:31 +0800

    reochen

--- files/calendar/data.js
+++ files/calendar/data.js
@@ -2,6 +2,16 @@
 $(document).ready(function(){
 	"use strict";
 	$("#selectedPage").css("top","772px");
+	var today=2;
+	var isToday=$(".isToday");
+	var haveEvents=$(".haveEvents");
+	for(var i=0;i<7;i++)
+		{
+			if(i!==today-1){
+				$(isToday[i]).hide();
+			}
+		}
+	$(haveEvents[0]).hide();$(haveEvents[2]).hide();$(haveEvents[3]).hide();$(haveEvents[6]).hide();
 	var mySwiper=new Swiper('.swiper-container',{
 		loop:true
 	});
