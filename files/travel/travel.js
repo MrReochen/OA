e96e47cad46911e6e0c2c216c256e3a5b5ae64a7
mmmmayi <657818050@qq.com>
2016-12-07 08:45:12 +0800

    mmmmayi

---
+++ files/travel/travel.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+$(document).ready(function(){
+	"use strict";
+	$("#menu").hide();
+	$("#pic5").click(function(){
+		 $("#menu").slideDown();
+	});
+	$("#word2").click(function(){
+		window.location.href="applyforreimbursement.html";
+	});
+	$("#word2").click(function(){
+		
+	});
+});
