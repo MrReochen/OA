0f0e4bc50154095c9406f002dfc9a537ccfe6544
Reo Chen <crlwxy@gmail.com>
2016-12-09 14:34:08 +0800

    reochen

---
+++ files/addCalendarEvent/data.js
@@ -0,0 +1,18 @@
+// JavaScript Document
+$(document).ready(function(){
+	"use strict";
+	$('#dd').calendar({
+        trigger: '#date',
+        zIndex: 999,
+		format: 'yyyy-mm-dd',
+        onSelected: function (view, date, data) {
+            console.log('event: onSelected')
+        },
+        onClose: function (view, date, data) {
+            console.log('event: onClose'),
+            console.log('view:' + view),
+            console.log('date:' + date),
+            console.log('data:' + (data || 'None'));
+        }
+    });
+});
