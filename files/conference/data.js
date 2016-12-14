99aec948d5de944f7b9445648ef2ded4ef7a2146
Reo Chen <crlwxy@gmail.com>
2016-12-14 14:43:01 +0800

    reochen

--- files/conference/data.js
+++ files/conference/data.js
@@ -5,4 +5,12 @@ $(document).ready(function(){
 	$("#myConferenceButton").click(function(){
 		window.location.href="conferenceraised.html";
 	});
+	var notification=$(".notification");
+	$(notification).click(function(){
+		window.location.href="conferencedetail.html";
+	});
+	var report=$(".report");
+	$(report).click(function(){
+		window.location.href="conferencereport.html";
+	});
 });
